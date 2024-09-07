c268376405dd3e0fa9cf940cc240806d5340321a
Sir-Muguna <mwendasir@gmail.com>
2024-09-07 13:08:29 +0300

    add stream project files

--- spark_stream.py
+++ spark_stream.py
@@ -1,46 +1,46 @@
 import logging
+import uuid
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
 from pyspark.sql.functions import from_json, col
-import uuid
+from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
 
-# Create keyspace if it doesn't exist
 def create_keyspace(session):
-    session.execute("""
-        CREATE KEYSPACE IF NOT EXISTS spark_streams
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
-    """)
-    logging.info("Keyspace 'spark_streams' created successfully!")
+    try:
+        session.execute("""
+            CREATE KEYSPACE IF NOT EXISTS spark_streams
+            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+        """)
+        logging.info("Keyspace created successfully!")
+    except Exception as e:
+        logging.error(f"Could not create keyspace due to: {e}")
 
-
-# Create the forex_table table if it doesn't exist
 def create_table(session):
-    session.execute("""
-    CREATE TABLE IF NOT EXISTS spark_streams.forex_table (
-        id UUID PRIMARY KEY,
-        ticker TEXT,
-        bid FLOAT,
-        ask FLOAT,
-        open_price FLOAT, 
-        low FLOAT,
-        high FLOAT,
-        changes FLOAT,
-        date TIMESTAMP
-    );
-    """)
-    logging.info("Table 'forex_table' created successfully!")
+    try:
+        session.execute("""
+        CREATE TABLE IF NOT EXISTS spark_streams.forex_table (
+            id UUID PRIMARY KEY,
+            ticker TEXT,
+            bid FLOAT,
+            ask FLOAT,
+            open_price FLOAT, 
+            low FLOAT,
+            high FLOAT,
+            changes FLOAT,
+            date TIMESTAMP
+        );
+        """)
+        logging.info("Table created successfully!")
+    except Exception as e:
+        logging.error(f"Could not create table due to: {e}")
 
-
-# Insert data into Cassandra
 def insert_data(session, **kwargs):
     logging.info("Inserting data...")
-
     ticker_id = uuid.uuid4()  # Generates a new UUID
     ticker_name = kwargs.get('ticker')
     bid = kwargs.get('bid')
     ask = kwargs.get('ask')
-    open_price = kwargs.get('open') 
+    open_price = kwargs.get('open')
     low = kwargs.get('low')
     high = kwargs.get('high')
     changes = kwargs.get('changes')
@@ -52,58 +52,52 @@ def insert_data(session, **kwargs):
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (ticker_id, ticker_name, bid, ask, open_price, low, high, changes, date))
         logging.info(f"Data inserted for {ticker_name} with bid {bid}")
-
     except Exception as e:
         logging.error(f"Could not insert data due to: {e}")
 
-
-# Create Spark session
 def create_spark_connection():
     try:
-        spark_conn = SparkSession.builder \
+        s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,"
-                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.1") \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.1,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,"
+                                           "org.apache.kafka:kafka-clients:3.5.1") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
 
-        spark_conn.sparkContext.setLogLevel("ERROR")
+        s_conn.sparkContext.setLogLevel("ERROR")
         logging.info("Spark connection created successfully!")
-        return spark_conn
+        return s_conn
     except Exception as e:
-        logging.error(f"Couldn't create the Spark session due to: {e}")
+        logging.error(f"Couldn't create the Spark session due to exception {e}")
         return None
 
-
-# Connect to Kafka topic
 def connect_to_kafka(spark_conn):
     try:
+        logging.info("Attempting to create Kafka DataFrame...")
         spark_df = spark_conn.readStream \
             .format('kafka') \
             .option('kafka.bootstrap.servers', 'localhost:9092') \
             .option('subscribe', 'forex_data') \
             .option('startingOffsets', 'earliest') \
             .load()
-        logging.info("Kafka dataframe created successfully.")
+        logging.info("Kafka DataFrame created successfully")
         return spark_df
     except Exception as e:
-        logging.error(f"Kafka dataframe could not be created due to: {e}")
+        logging.error(f"Failed to create Kafka DataFrame: {e}")
         return None
 
-
-# Create Cassandra connection
 def create_cassandra_connection():
     try:
+        # Connecting to the Cassandra cluster
         cluster = Cluster(['localhost'])
-        session = cluster.connect()
-        logging.info("Connected to Cassandra cluster.")
-        return session
+        cas_session = cluster.connect()
+        logging.info("Cassandra connection created successfully!")
+        return cas_session
     except Exception as e:
-        logging.error(f"Could not create Cassandra connection due to: {e}")
+        logging.error(f"Could not create Cassandra connection due to {e}")
         return None
 
-
-# Select relevant fields from Kafka stream
 def create_selection_df_from_kafka(spark_df):
     schema = StructType([
         StructField("ticker", StringType(), False),
@@ -123,11 +117,9 @@ def create_selection_df_from_kafka(spark_df):
         
         logging.info("Schema successfully applied to Kafka stream.")
         return sel
-
     except Exception as e:
         logging.error(f"Error applying schema to Kafka stream: {e}")
         return None
-
 
 if __name__ == "__main__":
     # Setup logging
